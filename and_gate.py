57e042611ca510083d00e11b4d8114166056225e
Ishikawa-battousai <ajomdd@gmail.com>
2024-03-11 15:33:17 +0100

    added gates

---
+++ and_gate.py
@@ -0,0 +1,11 @@
+def AND_gate(a, b):
+  if a and b:
+    return 1
+  return 0
+
+# TEST CASES
+
+print("A: 0, B: 0 | Output: {0}".format(AND_gate(0, 0)))
+print("A: 0, B: 1 | Output: {0}".format(AND_gate(0, 1)))
+print("A: 1, B: 0 | Output: {0}".format(AND_gate(1, 0)))
+print("A: 1, B: 1 | Output: {0}".format(AND_gate(1, 1)))
